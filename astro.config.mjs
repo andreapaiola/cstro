3d966e78fb237de296b517ff429af83ca9c49c44
Andrea Paiola <andrea.paiola@gmail.com>
2024-04-18 09:18:50 +0200

    fix css typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 
-//import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   integrations: [db()],
-  //adapter: cloudflare()
 });
